bf811d15c973dc4414ed065584e821cacc7ed80f
Zixu.Deng <admin@dengzixu.com>
2022-09-23 17:21:49 +0800

    🎉 初始化项目

---
+++ src/main/java/net/dengzixu/bilvetools/BilveToolsApplication.java
@@ -0,0 +1,13 @@
+package net.dengzixu.bilvetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilveToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilveToolsApplication.class, args);
+    }
+
+}
