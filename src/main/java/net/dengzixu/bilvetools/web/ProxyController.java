16785295be258a56749df72700bded9bb16f1480
Deng.Zixu <admin@dengzixu.com>
2023-01-09 15:21:18 +0800

    🐛 修复已知问题
    修复路由映射错误
    修复数据返回类型错误

--- src/main/java/net/dengzixu/bilvetools/web/ProxyController.java
+++ src/main/java/net/dengzixu/bilvetools/web/ProxyController.java
@@ -1,6 +1,7 @@
 package net.dengzixu.bilvetools.web;
 
 import net.dengzixu.bilvetools.api.bili.live.BLiveAPI;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,19 +17,25 @@ public class ProxyController {
 
     private final BLiveAPI bLiveAPI = new BLiveAPI();
 
-    @GetMapping("giftConfig")
+    @GetMapping("/giftConfig")
     public ResponseEntity<String> proxyGiftConfig() {
         try {
-            return ResponseEntity.ok(bLiveAPI.giftConfig());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            return ResponseEntity.ok().headers(headers).body(bLiveAPI.giftConfig());
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("");
         }
     }
 
-    @GetMapping("giftData")
+    @GetMapping("/giftData")
     public ResponseEntity<String> proxyGiftData(@RequestParam(name = "room_id") String roomID) {
         try {
-            return ResponseEntity.ok(bLiveAPI.giftData(Long.parseLong(roomID)));
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            return ResponseEntity.ok().headers(headers).body(bLiveAPI.giftData(Long.parseLong(roomID)));
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("");
         }
