ede2a074a78ed67929f315c1a445d3133cfc7bc3
Deng.Zixu <admin@dengzixu.com>
2023-03-05 23:45:58 +0800

    🎨 优化 Proxy 相关代码
    采用新的 proxy 策略，并移除老方案

--- src/main/java/net/dengzixu/bilvetools/web/ProxyController.java
+++ src/main/java/net/dengzixu/bilvetools/web/ProxyController.java
@@ -1,43 +1,62 @@
 package net.dengzixu.bilvetools.web;
 
-import net.dengzixu.bilvetools.api.bili.live.BLiveAPI;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/proxy")
 public class ProxyController {
+    // Looger
+    private static final org.slf4j.Logger Logger = org.slf4j.LoggerFactory.getLogger(ProxyController.class);
 
-    private final BLiveAPI bLiveAPI = new BLiveAPI();
 
-    @GetMapping("/giftConfig")
-    public ResponseEntity<String> proxyGiftConfig() {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Type", "application/json");
 
-            return ResponseEntity.ok().headers(headers).body(bLiveAPI.giftConfig());
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("");
+    @GetMapping("/**")
+    public ResponseEntity<String> proxy(HttpServletRequest httpServletRequest) {
+
+        // 获取 URI
+        String requestURI = httpServletRequest.getRequestURI();
+
+        // 判断 URL 中是否包含 api.live.bilibili.com
+        if (!requestURI.contains("api.live.bilibili.com")) {
+            Logger.error("[Proxy] URL: {} 不在白名单内", requestURI);
+            return ResponseEntity.status(403).body("Proxy URL 不在白名单内");
         }
-    }
 
-    @GetMapping("/giftData")
-    public ResponseEntity<String> proxyGiftData(@RequestParam(name = "room_id") String roomID) {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Content-Type", "application/json");
+        // 获取完整的 QueryString
+        String queryString = httpServletRequest.getQueryString();
 
-            return ResponseEntity.ok().headers(headers).body(bLiveAPI.giftData(Long.parseLong(roomID)));
+        // 去除 URI 中的 /proxy/
+        String biliAPI = requestURI.replaceFirst("/proxy/", "");
+
+        // 合成 URL
+        String fullURL = biliAPI + "?" + queryString;
+
+        // 发送请求
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        // 构建 Request
+        Request request = new Request.Builder().url(fullURL).get().build();
+
+        try (Response response = okHttpClient.newCall(request).execute()) {
+
+            return ResponseEntity.status(response.code())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response.body() != null ? response.body().string() : null);
+
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("");
+            Logger.error("[Proxy] 出现异常 ", e);
+            return ResponseEntity.status(503).build();
         }
     }
 }
